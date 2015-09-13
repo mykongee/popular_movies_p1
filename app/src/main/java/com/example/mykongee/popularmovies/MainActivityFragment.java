f6a081f91038d161df98094f504dd1a1080439d7
Mykongee <haibing.hong@mail.utoronto.ca>
2015-09-12 22:22:00 -0400

    Dynamically added views for trailers and reviews in DetailsFragment

--- app/src/main/java/com/example/mykongee/popularmovies/MainActivityFragment.java
+++ app/src/main/java/com/example/mykongee/popularmovies/MainActivityFragment.java
@@ -286,7 +286,7 @@ public class MainActivityFragment extends Fragment implements LoaderManager.Load
                     final String RESPONSE = "trailers,reviews";
 
                     // TODO take both API keys out when commiting
-                    final String API_KEY = "c99a4285b4e0a83397b9deca2e4d9d16";
+                    final String API_KEY = "";
 
                     Uri builtUri = Uri.parse(BASE_URL).buildUpon()
                             .appendQueryParameter(API_PARAM, API_KEY)
@@ -350,7 +350,7 @@ public class MainActivityFragment extends Fragment implements LoaderManager.Load
             //http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=[APIKEY]
 
             try {
-                final String API_KEY = "c99a4285b4e0a83397b9deca2e4d9d16";
+                final String API_KEY = "";
                 final String BASE_URL = "http://api.themoviedb.org/3/discover/movie";
                 final String SORT_PARAM = "sort_by";
                 final String API_PARAM = "api_key";
